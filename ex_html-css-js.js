778e586881fb5b9e37554b728f1bf886ea0f65ff
MarwanLC <mloucar895@g.educaand.es>
2025-03-21 14:01:05 +0100

    .

--- ex_html-css-js.js
+++ ex_html-css-js.js
@@ -1,7 +1,14 @@
 let nombre = document.getElementById("nombre");
 
-if(nombre)
+
+document.getElementById("instrucciones").oninput = function (){
+    document.getElementById("instrucciones").textContent = this.value.length;
+};
 
 document.getElementById("boton").onclick = function (){
     document.body.classList.toggle("oscuro");
+};
+
+document.getElementById("enviar").onmouseover = function (){
+    document.inputEncoding.classList.toggle("white");
 };
